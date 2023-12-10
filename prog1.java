280af0b9abfa78af9d45382a3e2c8cc29ed90836
panand11 <panand.sap@gmail.com>
2023-12-10 16:52:42 +0530

    Test Java programs

---
+++ prog1.java
@@ -0,0 +1,13 @@
+package src;
+
+public class prog1 {
+    public static void main(String[] args) {
+        int a=10;
+        float b= 100;
+        double c;
+        c= a%b;
+        char ch = 67;
+        System.out.println("result = " +c +'\t' + "Char value "+ch);
+
+    }
+}
